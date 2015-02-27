e1c2d6558f5cfdf95c5c2df3846045b4706942ee
tigo <tigo>
2015-02-27 22:03:55 +0000

    range test

--- src/main/java/number/printer/NumberToWord.java
+++ src/main/java/number/printer/NumberToWord.java
@@ -1,5 +1,12 @@
 package number.printer;
 
 public class NumberToWord {
+
+    public static String convertNumberToWord(int number) throws Exception {
+        if (number > 3999 || number < 1)
+            throw new Exception("expected range 1 : 3999 got " + number);
+        return null;
+    }
+
 }
 
--- src/test/java/number/printer/NumberToWordTest.java
+++ src/test/java/number/printer/NumberToWordTest.java
@@ -13,6 +13,31 @@ public class NumberToWordTest {
     private static final Logger logger = LogManager.getLogger("NumberToWordTest");
 
     @Test
+    public void rangeTest() throws Exception {
+
+        boolean result1 = false;
+        boolean result2 = false;
+
+        try {
+            NumberToWord.convertNumberToWord(0);
+        } catch (Exception e) {
+            logger.info("===== ok " + e.getMessage());
+            result1 = true;
+        }
+
+        try {
+            NumberToWord.convertNumberToWord(4000);
+        } catch (Exception e) {
+            logger.info("===== ok " + e.getMessage());
+            result2 = true;
+        }
+
+        if (result1 != true || result2 != true)
+            throw new Exception("range test failed");
+            
+    }
+
+    @Test
     public void numberToWordTest() {
         assertTrue(true);
     }
